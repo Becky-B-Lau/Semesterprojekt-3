d9faaeccb9c44de0a13c98cc118b809c372cae78
Maria <mariadybdahl@hotmail.dk>
2024-11-25 11:40:45 +0100

    Updated javascript

--- JavaScript.js
+++ JavaScript.js
@@ -1,13 +1,34 @@
 const{createApp} = vue;
 createApp({
     data(){
-        return calculate_percentage(200, 10000)
+        return {
+            steps: 200,
+            goal: 10000,
+            percentage: 0,
+            apiUrl: 'http://127.0.0.1:5000/calculate_percentage'
+        };
     },
     
     methods:{
+        async updatePercentage()
+        {
+            try
+            {
+                const respones = await axios.post(this.apiUrl,{
+                    current: this.steps,
+                    total: this.goal
+                });
+                this.percentage= respones.data.percentage;                
+            } catch(error){console.error('Error udregn procent:',error)};
+        },        
+        AddStep(amount){
+            this.steps +=amount
+            this.updatePercentage();
+        },
         Calculate_Goal(){
             this.calculate_percentage.push(200, 10000)
         }
-    }
+    },
+    mounted(){this.updatePercentage();}
 
 }).mount('#app');
--- daily goal.py
+++ daily goal.py
@@ -1,5 +1,19 @@
-def calculate_percentage(current_value, totale_value):
-    if taltal_value>0:
-        return (current_value / totale_value) * 100
-    return 0
+from flask import flask, jsonify, request
+
+app=Flask(__name__)
+
+@app.route('/calculate_percentage', methods=['POST'])
+
+
+def calculate_percentage():
+    data = request.json
+    current=data.get('current',0)
+    total=data.get('total',1)
+
+    if total ==0:
+        return jsonify({"error": "total kan ikke være 0"}),400
+    percentage=(current/total)*100
+    return jsonify({"percentage":round(percentage,2)})
+if __name__=='__main__':
+    app.run(debug=True) 
 
