80c1794b68f9f6c591cd7da7e39d38f6b98e1f14
Maria <mariadybdahl@hotmail.dk>
2024-11-27 13:37:08 +0100

    Update index og rest virker

--- RESTVisePåHjemmesiden.py
+++ RESTVisePåHjemmesiden.py
@@ -1,16 +1,22 @@
 from flask import Flask, jsonify
 import Database
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
+CORS(app)
 
 # Server index-siden
+
 @app.route('/', methods=['GET'])
 def index():
     step = Database.read_last_step()
-    print(f"Step returned by database: {step}")  # Debug
     if step is not None:
-        return jsonify({"step": step})  # Return JSON-data
+        print(f"Step returned by database: {step}")  # Debug i Flask-konsollen
+        return jsonify({"step": step})  # Returner JSON
     else:
-        print("No data found in database.")  # Debug
+        print("No data found in database.")  # Debug i Flask-konsollen
         return jsonify({"error": "No data found"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
