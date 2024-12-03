b558b7cd090f6e4a11270445b6edc7b0a4a1f74a
Becky-B-Lau <Bblau@outlook.dk>
2024-12-03 14:19:05 +0100

    Standard citat virker

--- Citat.js
+++ Citat.js
@@ -1,33 +1,32 @@
 const QUOTE_API_URL = "https://zenquotes.io/api/random"; // Eksempel API til citater
 
-const appQuote = Vue.createApp ({
+const appQuote = Vue.createApp({
     data() {
-            return {
-                date: new Date().toLocaleDateString(), // Standard dato
-                quoute: "Don't be afraid of death", // Standard fase (hentes fra databasen)
-                author: "Minato" 
-            };
-        },
-        methods: {
-            async fetchQuote() {
-                try {
-                    // Fetch citat fra din backend
-                    const response = await fetch('http://127.0.0.1:5001'); // Ændr URL til din endpoint
-                    const data = await response.json();
+        return {
+            date: new Date().toLocaleDateString(), // Standard dato
+            quote: "Don't be afraid of death", // Standard fase (hentes fra databasen)
+            author: "Minato"
+        };
+    },
+    methods: {
+        async fetchQuote() {
+            try {
+                // Fetch citat fra din backend
+                const response = await fetch('http://127.0.0.1:5001/quote'); // Ændr URL til din endpoint
+                
+                const data = await response.json();
 
-                    // Opdater dataen
-                    this.date = data.date; 
-                    this.quote = data.quote; 
-                    this.author = data.author;
-                } catch (error) {
-                    console.error('Fejl ved hentning af citat:', error);
-                }
-              }
-            },
-            mounted() {
-                this.fetchQuote();
-    
-                setInterval(this.fetchQuote, 86400000);
+                // Opdater dataen
+                this.date = data.date;
+                this.quote = data.quote;
+                this.author = data.author;
+            } catch (error) {
+                console.error('Fejl ved hentning af citat:', error);
             }
-
+        }
+    },
+    mounted() {
+        this.fetchQuote();
+        setInterval(this.fetchQuote, 86400000);
+    }
 }).mount('#appQuote');
