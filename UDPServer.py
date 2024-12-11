0d394202f3b422dd1a02a090bffcf5f98b1d9403
Maria <mariadybdahl@hotmail.dk>
2024-12-11 10:13:47 +0100

    Update - med 15

--- UDPServer.py
+++ UDPServer.py
@@ -9,14 +9,7 @@ serverAddress = ('', serverPort)
 serverSocket.bind(serverAddress)
 
 print("The server is ready to receive")
-# Definer tid for nulstilling (f.eks. kl. 00)
-reset_time = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-if reset_time < datetime.now():
-    # Hvis tidspunktet allerede er passeret i dag, sæt det til næste dag
-    reset_time += timedelta(days=1)
 
-# Definer en margin på 1 minut
-margin = timedelta(seconds=30)
 stepFromDatabase = Database.read_last_step()
 if stepFromDatabase is None:
     stepFromDatabase = 0  # Standardværdi for stepFromDatabase
@@ -34,19 +27,10 @@ if Coutner_tree is None:
 
 try:
     while True:
-        now = datetime.now()
         # Modtag data fra klienten
         message, clientAddress = serverSocket.recvfrom(2048)
         decoded_message = message.decode()
-        
-            # Tjek om det er midnat
-        if reset_time - margin <= now <= reset_time + margin:
-            print(f"Nulstillingstidspunkt nået eller inden for margin: {reset_time}")
-            stepFromDatabase = 0  # Nulstil skridttælleren
-            print("Skridttæller nulstillet.")
-            
-            # Sæt reset_time til næste dag
-            reset_time += timedelta(days=1)
+
         try:
             # Konverter beskeden til integer (trin tælling)
             step_count = int(decoded_message)
@@ -58,15 +42,15 @@ try:
                 phase+=1
                 if (phase>5):
                     phase=0
+                    
             if (phase==5):
                     last_phase = Database.read_last_phase()
                     if last_phase != 5:  # Tjek om fasen netop er blevet 5
                         Coutner_tree += 1
-            
+                        
             # Gem beskeden i tabellen "steps"
             Database.insert_data(stepFromDatabase,phase, datetime.now(),Coutner_tree)
            
-
         except ValueError:
             print(f"Invalid data received: {decoded_message}. Skipping...")
             serverSocket.sendto(b"Invalid data format.", clientAddress)
